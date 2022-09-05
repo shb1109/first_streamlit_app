8ce949d9c7a7b0b9877886a22e7160813961d471
Shubham Jain <112896416+shb1109@users.noreply.github.com>
2022-09-05 23:33:06 +0530

    Update streamlit_app.py
    Avocado added

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥣 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑Avocado Toast')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
