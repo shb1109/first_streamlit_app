90c4589d2d17190fd54c90eab8ddff36e73974b7
Shubham Jain <112896416+shb1109@users.noreply.github.com>
2022-09-05 23:18:10 +0530

    Update streamlit_app.py
    fixed texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner!')
 
-streamlit.header('Brealfast Menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard Boiled free ranged-eggs')
+streamlit.text('Hard-Boiled Free-Range Egg')
